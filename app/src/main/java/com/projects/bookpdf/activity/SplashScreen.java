9248ba17306a7918468516e2bb107c8f9fc42d77
Ashish mehta <ashish11mehta11@gmail.com>
2020-04-29 13:29:31 +0530

    Initial Commit

---
+++ app/src/main/java/com/projects/bookpdf/activity/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.projects.bookpdf.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.projects.bookpdf.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+    }
+}
